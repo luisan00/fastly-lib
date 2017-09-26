cad344143885c478b387fce3d1c1154551038a59
luisan00 <luisan00@MacBook-Pro-de-Luis.local>
2017-09-26 16:49:51 +0200

    Update tests

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ const tap = require('tap').test;
 const fastlylib = require('../lib/fastlyJS.js') ;
 
 tap('fastly-js', function(test) {
-	var fastly = new fastlylib('fastly-api-key')
-	test.type(fastly, 'Object', 'Returns an Object')
+	var fastly = new fastlylib('fastly-api-key');
+
+	test.type(fastly, 'object', 'Returns an Object')
 });
