e00d44ba46d78d5b81d02b4faa3c520a724cbdb7
luisan00 <luisan00@MacBook-Pro-de-Luis.local>
2017-09-26 15:02:45 +0200

    Updated devDependencies in package.json

--- test/index.js
+++ test/index.js
@@ -1,1 +1,7 @@
-var test = require('tap')
+const tap = require('tap');
+const fastlyjs = require('../lib/fastlyJS.js') ;
+
+tap('fastly-js', function(test) {
+	var fastly = new fastlyjs('fastly-api-key')
+	test.type(fastly, 'Object', 'Returns an Object')
+});
