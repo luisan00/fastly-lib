7e219aebe9b98442316c0151c13c19e7163574f8
luisan00 <luisan00@MacBook-Pro-de-Luis.local>
2017-09-28 14:42:47 +0200

    update

--- test/tests.js
+++ test/tests.js
@@ -2,9 +2,9 @@ const tap = require('tap');
 const fastly = require('../lib/fastly.js');
 const fastly_api_key =  process.env.FASTLY_API_KEY || ''
 
-tap.test('fastly constructor', function(t){
-	var flib = new fastly(fastly_api_key);
+var flib = new fastly(fastly_api_key);
 
+tap.test('fastly constructor', function(t){
 	t.type(flib.request, 'object', 'request => object');
 	t.type(flib.request.options, 'object', '.options => object');
 	t.match(flib.request.options, {
@@ -24,14 +24,17 @@ tap.test('fastly constructor', function(t){
 })
 
 tap.test('utilities', function(t) {
+	t.type(flib.content, 'function', '.content() => is function')
+		.then((res)=>{
+			t.type(res, 'function', '.content().then => return function done.')
+		})
+		.catch((err)=>{
+			t.type(res, 'function', '.content().catch => return function error.')
+		})
 
-	var flib = new fastly(fastly_api_key);
-
-	t.type(flib, 'object', 'constructor');
-	t.type(flib.content, 'function', 'method content');
-	t.type(flib.datacenters, 'function', 'method datacenters');
-	t.type(flib.docs, 'function', 'method docs');
-	t.type(flib.public_ip_list, 'function', 'method public_ip_list');
+	t.type(flib.datacenters, 'function', 'datacenters() => is function');
+	t.type(flib.docs, 'function', 'docs() => is function');
+	t.type(flib.public_ip_list, 'function', 'public_ip_list() => is function');
 	t.end();
 });
 
