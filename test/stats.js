3e6d36bed472f143d701c6e61fff3c6b375d71ea
luisan00 <luisan00@MacBook-Pro-de-Luis.local>
2017-09-29 18:15:06 +0200

    New test added to stats method

--- test/stats.js
+++ test/stats.js
@@ -1,22 +1,37 @@
 const tap = require('tap');
 const fastly = require('../lib/fastly.js');
-const fastly_api_key =  process.env.FASTLY_API_KEY || '';
+const fastly_api_key = process.env.FASTLY_API_KEY || '';
 
 var flib = new fastly(fastly_api_key);
 
-// --> .stats() no params
-tap.test('.stats()', function(t) {
-	t.type(flib.stats, 'function', 'return function')
-	t.end();
-});
+// --> .stats() wo.params
 tap.test('.stats.then()', function(t) {
-	flib.stats()
-		.then((res)=>{
-			t.type(res, 'object', 'return object');
-			t.end();
-		})
-		.catch((err)=>{
-			t.fail(err);
-			t.end();
-		})
+    t.type(flib.stats, 'function', 'return function')
+    flib.stats()
+        .then((res) => {
+            t.type(res, 'object', 'return object');
+            t.end();
+        })
+        .catch((err) => {
+            t.fail(err);
+            t.end();
+        })
+});
+
+// --> .stats() w.params
+tap.test('.stats.then()', function(t) {
+    t.type(flib.stats, 'function', 'return function')
+    flib.stats({
+            from: 'Yesterday',
+            to: 'Today',
+            by: 'Hour'
+        })
+        .then((res) => {
+            t.type(res, 'object', 'return object');
+            t.end();
+        })
+        .catch((err) => {
+            t.fail(err);
+            t.end();
+        })
 });
