6d02be2c33ca53354384ac423e5d353f5fa5c559
luisan00 <luisan00@hotmail.com>
2019-06-09 19:13:19 +0200

    Update purge.js

--- test/purge.js
+++ test/purge.js
@@ -1,23 +1,11 @@
-const tap = require('tap');
+const test = require('tap');
 const fastly = require('../lib/fastly.js');
 const fastly_api_key =  process.env.FASTLY_API_KEY || '';
 const content_url =  process.env.CONTENT_URL || '';
 
 var flib = new fastly(fastly_api_key);
 
-// --> .purge()
 tap.test('.purge()', function(t) {
 	t.type(flib.purge, 'function', 'return function')
 	t.end();
-});
-tap.test('.purge.then()', function(t) {
-	flib.purge(content_url)
-		.then((res)=>{
-			t.type(res, 'object', 'return object');
-			t.end();
-		})
-		.catch((err)=>{
-			t.fail(err);
-			t.end();
-		})
 });
