6c47a1417171df1d33dfe58b768cb533c66797e4
luisan00 <luisan00@hotmail.com>
2022-07-27 11:00:51 +0200

    update tests

--- test/purge.js
+++ test/purge.js
@@ -5,7 +5,21 @@ const content_url =  process.env.CONTENT_URL || '';
 
 var flib = new fastly(fastly_api_key);
 
+// .purge()
 tap.test('.purge()', function(t) {
 	t.type(flib.purge, 'function', 'return function')
 	t.end();
+});
+
+// .purge() => then()
+tap.test('.purge.then()', function(t) {
+	flib.purge(content_url)
+		.then((res)=>{
+			t.type(res, 'string', 'return string');
+			t.end();
+		})
+		.catch((err)=>{
+			t.fail(err);
+			t.end();
+		})
 });
