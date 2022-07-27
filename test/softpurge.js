f3ed30e2985ebd13916a37147b539591fea7e6fc
luisan00 <luisan00@hotmail.com>
2022-07-27 11:06:21 +0200

    update softpurge test

--- test/softpurge.js
+++ test/softpurge.js
@@ -5,7 +5,21 @@ const content_url =  process.env.CONTENT_URL || '';
 
 var flib = new fastly(fastly_api_key);
 
+// softpurge
 tap.test('.softpurge()', function(t) {
 	t.type(flib.softpurge, 'function', 'return function');
 	t.end();
+});
+
+// softpurge => then
+tap.test('.softpurge.then()', function(t) {
+	flib.softpurge(content_url)
+		.then((res)=>{
+			t.type(res, 'string', 'return string');
+			t.end();
+		})
+		.catch((err)=>{
+			t.fail(err);
+			t.end();
+		})
 });
